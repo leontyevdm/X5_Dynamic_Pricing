3e0f1a3143901e7c6f0a9faa926f01527c9869fb
Artur Slepnev <slepnev.am>
2021-04-09 20:12:50 +0300

    Issue ID: XDP-39 Dynamic Pricing (implemented backend)

---
+++ model.py
@@ -0,0 +1,11 @@
+
+
+
+class PricePredictor():
+    def fit(self):
+        pass
+
+    def predict(self,origin,dest,current_date,flight_date):
+        """returns two lists of days (like [07.04.21,08.04.21,09.04.21]) and prices ( list of prices
+        predicted for this days """
+        return ([],[])
