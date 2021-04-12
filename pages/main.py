4fe3adf0d5bfa84b196c06f259e96f87c0cfd1ec
Artur Slepnev <slepnev.am>
2021-04-12 18:02:43 +0300

    Issue ID: XDP-39 Dynamic Pricing (implemented backend)

--- pages/main.py
+++ pages/main.py
@@ -1,11 +1,2 @@
-from globals import get_app
-import  json
-from pages.mod_control_page import mod_control_page
-from pages.db_page import db_page
-from pages.main_page import main_page
-from pages.ml_model_page import ml_model_page
 
-get_app().register_blueprint(ml_model_page)
-get_app().register_blueprint(db_page)
-get_app().register_blueprint(main_page)
 
