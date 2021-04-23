2f68a29668ac0c4d7372a6ec9f4a562c824c7bda
Pavel Paramonov <paramonov.pp@phystech.edu>
2021-04-23 05:24:47 +0300

    XDP-43

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from globals import app as application
 import pages
 from tools.mods import get_flag_LOCAL_MODE
 from tools.log import logger
+from model import fit_cached_models
 import sys
+from globals import *
 from model import PricePredictor
+from datetime import datetime as date
+
 @application.errorhandler(Exception)
 def handle_error(error):
     if get_flag_LOCAL_MODE():
@@ -21,7 +25,18 @@ def handle_error(error):
 
 @application.route('/', methods=['GET'])
 def describe():
+    # cache models test
+    origin = 'MOW'
+    dest = 'LED'
+    # '%d.%m.%y'
+    current_date = '12.4.21'
+    current_date = date.strptime(current_date, '%d.%m.%y')
+    flight_date = '30.4.21'
+    flight_date = date.strptime(flight_date, '%d.%m.%y')
+    print(get_model().predict_actual_queried_prices_for(origin, dest, current_date, flight_date, 3, None))
     return 'Hello'
 
+
 if __name__ == "__main__":
+    fit_cached_models()
     application.run(port=5000 if len(sys.argv) == 1 else int(sys.argv[1]), threaded=True)
--- model.py
+++ model.py
@@ -13,20 +13,57 @@ from sktime.performance_metrics.forecasting import sMAPE, smape_loss
 from sktime.forecasting.exp_smoothing import ExponentialSmoothing
 from sktime.forecasting.arima import ARIMA, AutoARIMA
 
+cached_models = {}
+
+
+def get_cached_models():
+    global cached_models
+    return cached_models
+
+def get_prepared_df():
+    date_parser = lambda ts: datetime.strptime(ts, "%y-%m-%dT%H:%M:%SZ")
+    df = pd.read_csv('aviasales_data_t.csv')
+    df['requested_at'] = pd.to_datetime(df['requested_at'], format="%y-%m-%dT%H:%M:%SZ", errors="coerce")
+    df['departure_at'] = pd.to_datetime(df['departure_at'], format="%Y-%m-%dT%H:%M:%SZ", errors="coerce")
+    df['expires_at'] = pd.to_datetime(df['expires_at'], format="%Y-%m-%dT%H:%M:%SZ", errors="coerce")
+    df['price'] = pd.to_numeric(df["price"], errors="coerce")
+    df = df.dropna()
+    df = df.drop_duplicates(subset=['departure_at', 'expires_at', 'airline', 'flight_number', 'price'],
+                            keep='first')
+    return df
+
+
+def fit_forecast_model(orgn, dest, n_days, df):
+    orgn_dest = df[(df["origin"] == orgn) & (df["destination"] == dest)
+                   & (df["requested_at"] >= df["departure_at"] - datetime.timedelta(days=n_days + 1))
+                   & (df["requested_at"] <= df["departure_at"] - datetime.timedelta(days=n_days))]
+    # print(orgn_dest)
+
+    orgn_dest_day_min = orgn_dest.resample('D', on='requested_at')['price'].min()
+
+    orgn_dest_day_min = orgn_dest_day_min.fillna(orgn_dest_day_min.mean())  # not very smart
+
+    y = orgn_dest_day_min
+    forecaster = AutoARIMA()
+    forecaster.fit(y)
+    return forecaster
+
+
+def fit_cached_models():
+    airports = ['MOW', 'LED', 'KZN'] #not all
+    df = get_prepared_df()
+    for orig in airports:
+        for dest in airports:
+            if orig != dest:
+                print(orig, dest)
+                get_cached_models()[orig + dest] =\
+                    [fit_forecast_model(orig, dest, n_days, df) for n_days in range(7)]
+
 
 class PricePredictor():
 
     def prepare_df(self):
-        date_parser = lambda ts: datetime.strptime(ts, "%y-%m-%dT%H:%M:%SZ")
-        df = pd.read_csv('aviasales_data_t.csv')
-        df['requested_at'] = pd.to_datetime(df['requested_at'], format="%y-%m-%dT%H:%M:%SZ", errors="coerce")
-        df['departure_at'] = pd.to_datetime(df['departure_at'], format="%Y-%m-%dT%H:%M:%SZ", errors="coerce")
-        df['expires_at'] = pd.to_datetime(df['expires_at'], format="%Y-%m-%dT%H:%M:%SZ", errors="coerce")
-        df['price'] = pd.to_numeric(df["price"], errors="coerce")
-        df = df.dropna()
-        df = df.drop_duplicates(subset=['departure_at', 'expires_at', 'airline', 'flight_number', 'price'],
-                                keep='first')
-        return df
+        return get_prepared_df()
 
     def predict_queried_prices_for(self,orgn, dest, current_date, flight_date, n_days, df):
         orgn_dest = df[(df["origin"] == orgn) & (df["destination"] == dest) & (df["departure_at"] <= flight_date)
@@ -61,6 +98,14 @@ class PricePredictor():
             days.append((flight_date - datetime.timedelta(days=i)).strftime("%Y-%m-%d"))
             predictions.append(self.predict_queried_prices_for(orgn, dest, current_date, flight_date, i, df))
         return days, predictions
+
+    def predict_actual_queried_prices_for(self, orgn, dest, current_date, flight_date, n_days, df):
+        forecaster = get_cached_models()[orgn + dest][n_days]
+        pred_date = flight_date - datetime.timedelta(days=n_days)
+        fh = ForecastingHorizon(pd.DatetimeIndex([pred_date]), is_relative=False)
+        y_pred = forecaster.predict(fh)
+        return y_pred[0]
+
 
     def show_real_prices(self,orgn, dest, current_date, flight_date, delta_days=7):
         flight_date= date.strptime(flight_date, '%d.%m.%y')
